6eed43000432ed638714c201dde35ea4ef9b6c52
Lynchy <dan@lynchy.co.nz>
2020-05-18 00:20:55 +1000

    update reverse proxy

--- deploy_project.sh
+++ deploy_project.sh
@@ -10,3 +10,13 @@ git reset --hard origin/master
 docker-compose -f docker-compose.prod.yml down
 
 docker-compose -f docker-compose.prod.yml up -d --build
+
+docker network create portfoliotracker
+
+docker network connect portfoliotracker reverseproxy
+
+docker network connect portfoliotracker frontend-prod
+
+docker network connect portfoliotracker graphql
+
+docker network connect portfoliotracker db
